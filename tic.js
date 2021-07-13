ecc37f0de14567a30ab067add62934c15414b41c
mosky <smartmosky@gmail.com>
2021-07-13 17:59:59 -0400

    board display is based on array

--- tic.js
+++ tic.js
@@ -6,13 +6,15 @@ const gameboard = (() => {
     const x = 'X'
     const o = 'O'
     let ticBoard = document.querySelector('#ticBoard')
+    const board = [x, o, x, o, x, x, x, o, o ]
     for (let i=0; i<9; i++) {
         let box = document.createElement('div');
         box.classList.add('ticBoxDiv');
+        box.textContent = `${board[i]}`
 ticBoard.appendChild(box);
 
     }
-    const board = [x, o, x, o, x, x, x, o, o ]
+    
     
     
   })();
