c5e99eef8740511109a4d2976e42551248ab857e
mosky <smartmosky@gmail.com>
2021-07-13 20:53:32 -0400

    Boxes don't change when already filled in

--- tic.js
+++ tic.js
@@ -15,30 +15,35 @@ const player = (name, letter) => {
 
 const gameboard = (() => {
     
-    let ticBoard = document.querySelector('#ticBoard')
-    const board = [, , , , , , , ,  ]
+  let ticBoard = document.querySelector('#ticBoard')
+  const board = [, , , , , , , , , ]
 
-    let currentPlayer = player1;
+  let currentPlayer = player1;
 
     //This function switches player
     const switchPlayer = () => {
     if (currentPlayer.letter === player1.letter)
     {currentPlayer = player2} else {
       currentPlayer = player1
-    }};
+    }
+    console.log(board)
+  };
 
     //This loop creates the board pieces
     for (let i=0; i<9; i++) {
         let box = document.createElement('div');
         box.classList.add('ticBoxDiv');
+
+        //This is what the boxes do when clicked
         box.addEventListener('click', () => {
-             board[i] = currentPlayer.letter;
-            
+         if (board[i] !== undefined) {} else {
+          board[i] = currentPlayer.letter;
           switchPlayer()
-          
-            box.textContent = `${board[i]}`
-        })
-        ticBoard.appendChild(box);
+          box.textContent = `${board[i]}`}
+          })
+        
+          ticBoard.appendChild(box);
+          console.log(board)
 }
 })();
 
