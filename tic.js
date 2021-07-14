be344b6d854c85f815cbbf9e9b6b14ce391ea98a
mosky <smartmosky@gmail.com>
2021-07-13 20:38:01 -0400

    player changes each time a box is clicked

--- tic.js
+++ tic.js
@@ -1,22 +1,45 @@
 
+const player = (name, letter) => {
+  return {name, letter}
+    };
+  
+    
+  
+    
+   const player1 = player('jim', 'X');
+   const player2 = player('bob', 'O');
+   
+   
 
 
 
 const gameboard = (() => {
-    const x = 'X'
-    const o = 'O'
+    
     let ticBoard = document.querySelector('#ticBoard')
-    const board = [x, o, x, o, x, x, x, o, o ]
+    const board = [, , , , , , , ,  ]
+
+    let currentPlayer = player1;
+
+    //This function switches player
+    const switchPlayer = () => {
+    if (currentPlayer.letter === player1.letter)
+    {currentPlayer = player2} else {
+      currentPlayer = player1
+    }};
+
+    //This loop creates the board pieces
     for (let i=0; i<9; i++) {
         let box = document.createElement('div');
         box.classList.add('ticBoxDiv');
-        box.textContent = `${board[i]}`
-ticBoard.appendChild(box);
+        box.addEventListener('click', () => {
+             board[i] = currentPlayer.letter;
+            
+          switchPlayer()
+          
+            box.textContent = `${board[i]}`
+        })
+        ticBoard.appendChild(box);
+}
+})();
 
-    }
-    
-    
-    
-  })();
-
-  
+ 
